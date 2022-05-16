e2d4a89ceb658417eb2f514e6190b092b7b4d308
Zadiraaj <mughalzahid462@gmail.com>
2022-05-16 08:46:14 +0500

    make route for order page

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -4,6 +4,7 @@
      <div class="col-sm-10">
         <div class="trending-wrapper">
             <h4>Result for Products</h4><br>
+            <a class="btn btn-success" href="ordernow">Order Now</a> <br> <br>
             @foreach($products as $item)
             <div class=" row searched-item cart-list-devider">
              <div class="col-sm-3">
@@ -22,8 +23,8 @@
              </div>
             </div>
             @endforeach
+            <a class="btn btn-success" href="ordernow">Order Now</a> <br> <br>
           </div>
-          <a class="btn btn-success" href="ordernow">Order Now</a> <br> <br>
 
      </div>
 </div>
